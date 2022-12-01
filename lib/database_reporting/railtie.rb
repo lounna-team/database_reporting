b928714d093221308aef6fe63d30a0f66f868c1f
Benjamin Eycken <benjamin.eycken@hoggo.com>
2022-12-01 16:43:06 +0100

    done

--- lib/database_reporting/railtie.rb
+++ lib/database_reporting/railtie.rb
@@ -2,6 +2,7 @@ require 'database_reporting/presenter/database_reporting_presenter'
 require "database_reporting/services/database_sizing/gathering_data_table"
 require "database_reporting/services/database_sizing/database_size_service"
 require "database_reporting/services/database_sizing/table_size_service"
+require "database_reporting/services/database_sizing/column_data_service"
 
 module DatabaseReporting
   class Railtie < ::Rails::Railtie
